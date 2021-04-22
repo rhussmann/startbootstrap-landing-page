0dd67a01138e3c1cfd24579c639b505ecaa799b2
Ricky Hussmann <ricky.hussmann@gmail.com>
2021-04-21 20:55:20 -0400

    Add event listener for sign-up button

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,2 +1,9 @@
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    let submitButton = document.getElementById("submit-button");
+    submitButton.addEventListener("click", function () {
+	alert("Hello, there!");
+    });
+});
