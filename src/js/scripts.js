af58c0b3f8e67b8a799ab111feeb166c56256ec3
Ricky Hussmann <ricky.hussmann@gmail.com>
2021-04-21 21:31:26 -0400

    ?

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,6 +4,16 @@
 document.addEventListener("DOMContentLoaded", (event) => {
     let submitButton = document.getElementById("submit-button");
     submitButton.addEventListener("click", function () {
-	alert("Hello, there!");
+	let emailAddressBox = document.getElementById("email-address");
+	let emailAddress = emailAddressBox.value;
+	console.log("Submitting email", emailAddress);
+    
+	fetch("https://crm.rhussmann.com/api/v1/LeadCapture/12c476975d7a2b47b58941c994958354", {
+	    method: "POST",
+	    headers: {
+		"Content-Type": "application/json"
+	    },
+	    body: JSON.stringify({"emailAddress": emailAddress})
+	}).then(() => alert("Submitted!")).catch((err) => alert(err));
     });
 });
